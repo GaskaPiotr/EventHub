8c928bd40ed17f15792adecc2983543ea014e261
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 15:42:17 +0200

    fet: add send ticket confirmation method

--- src/main/java/com/github/gaskapiotr/eventhub/notification/management/NotificationManagement.java
+++ src/main/java/com/github/gaskapiotr/eventhub/notification/management/NotificationManagement.java
@@ -1,7 +1,18 @@
 package com.github.gaskapiotr.eventhub.notification.management;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 class NotificationManagement {
+
+    void sendTicketConfirmation(String email, String workshopTitle, Long ticketId) {
+        log.info("=====================================");
+        log.info("SENDING EMAIL TO : {}", email);
+        log.info("SUBJECT : Ticket Confirmed - {}", workshopTitle);
+        log.info("BODY : Thank you for your purchase!");
+        log.info("Your official Ticket ID is #{}.", ticketId);
+        log.info("=====================================");
+    }
 }
