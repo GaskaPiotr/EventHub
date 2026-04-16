6ada637c3a8032439eb909ec40425f0edbf29a85
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 09:27:55 +0200

    fet: add workshop dto properties
    Adds to workshop dto id, title, description, date, max capacity and
    current attendees

--- src/main/java/com/github/gaskapiotr/eventhub/catalog/WorkshopDTO.java
+++ src/main/java/com/github/gaskapiotr/eventhub/catalog/WorkshopDTO.java
@@ -1,4 +1,12 @@
 package com.github.gaskapiotr.eventhub.catalog;
 
-public record WorkshopDTO() {
-}
+import java.time.LocalDateTime;
+
+public record WorkshopDTO(
+        Long id,
+        String title,
+        String description,
+        LocalDateTime date,
+        int maxCapacity,
+        int currentAttendees
+) {}
