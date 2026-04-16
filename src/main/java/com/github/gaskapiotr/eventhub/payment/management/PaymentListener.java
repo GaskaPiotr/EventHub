c1915dc7a84b77e13deb19177f2dec5b576b4f80
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 15:51:48 +0200

    fet: add on event method
    Adds on event method and payment management field

--- src/main/java/com/github/gaskapiotr/eventhub/payment/management/PaymentListener.java
+++ src/main/java/com/github/gaskapiotr/eventhub/payment/management/PaymentListener.java
@@ -1,7 +1,21 @@
 package com.github.gaskapiotr.eventhub.payment.management;
 
+import com.github.gaskapiotr.eventhub.ticketing.TicketPurchasedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 class PaymentListener {
+    private final PaymentManagement paymentManagement;
+
+    @ApplicationModuleListener
+    void on(TicketPurchasedEvent event) {
+        paymentManagement.processPayment(
+                event.ticketId(),
+                event.attendeeEmail(),
+                event.price()
+        );
+    }
 }
