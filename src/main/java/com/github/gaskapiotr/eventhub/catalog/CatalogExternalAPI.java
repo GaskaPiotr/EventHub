87d8546e4d347e93eb576ba35b1a016e0b7b874f
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 09:44:31 +0200

    fet: adds two methods to external api
    Adds get all workshops and create workshop methods to external api

--- src/main/java/com/github/gaskapiotr/eventhub/catalog/CatalogExternalAPI.java
+++ src/main/java/com/github/gaskapiotr/eventhub/catalog/CatalogExternalAPI.java
@@ -1,4 +1,8 @@
 package com.github.gaskapiotr.eventhub.catalog;
 
+import java.util.List;
+
 public interface CatalogExternalAPI {
+    List<WorkshopDTO> getAllWorkshops();
+    WorkshopDTO createWorkshop(WorkshopDTO dto);
 }
