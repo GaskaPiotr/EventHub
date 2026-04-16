fa8eac646ef43d03ea68ff87b6ee31c766132c82
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 14:33:55 +0200

    fet: add boilerplate to ticketing management
    Adds implementation of external api and boilerplate for buy ticket
    Also adds fields repository, mapper, event publisher and catalog
    internal api.

--- src/main/java/com/github/gaskapiotr/eventhub/ticketing/management/TicketingManagement.java
+++ src/main/java/com/github/gaskapiotr/eventhub/ticketing/management/TicketingManagement.java
@@ -1,7 +1,26 @@
 package com.github.gaskapiotr.eventhub.ticketing.management;
 
+import com.github.gaskapiotr.eventhub.catalog.CatalogInternalAPI;
+import com.github.gaskapiotr.eventhub.ticketing.TicketDTO;
+import com.github.gaskapiotr.eventhub.ticketing.TicketingExternalAPI;
+import com.github.gaskapiotr.eventhub.ticketing.mapper.TicketMapper;
+import com.github.gaskapiotr.eventhub.ticketing.repository.TicketRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class TicketingManagement {
+@RequiredArgsConstructor
+public class TicketingManagement implements TicketingExternalAPI {
+    private final TicketRepository repository;
+    private final TicketMapper mapper;
+    private final ApplicationEventPublisher eventPublisher;
+    private final CatalogInternalAPI catalogInternalAPI;
+
+    @Override
+    @Transactional
+    public TicketDTO buyTicket(Long workshopId, String attendeeEmail) {
+        // TODO
+    }
 }
