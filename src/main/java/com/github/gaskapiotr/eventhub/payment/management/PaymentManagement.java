fdc98a894b09590268da6e3d1153f4a21a521cc0
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 15:49:25 +0200

    fet: add process payment method

--- src/main/java/com/github/gaskapiotr/eventhub/payment/management/PaymentManagement.java
+++ src/main/java/com/github/gaskapiotr/eventhub/payment/management/PaymentManagement.java
@@ -1,7 +1,20 @@
 package com.github.gaskapiotr.eventhub.payment.management;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
+@Slf4j
 @Service
 class PaymentManagement {
+
+    void processPayment(Long ticketId, String email, BigDecimal price) {
+        log.info("=======================================");
+        log.info("INITIATING PAYMENT FOR TICKET ID : {}", ticketId);
+        log.info("CHARGING ACCOUNT : {}", email);
+        log.info("AMOUNT : ${}", price);
+        log.info("STATUS : SUCCESS [MOCKED]");
+        log.info("=======================================");
+    }
 }
