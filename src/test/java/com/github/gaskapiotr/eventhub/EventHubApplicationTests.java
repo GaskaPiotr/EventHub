824ad0f652e94dfc4da9ceb0d302367dc79c0239
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 16:34:53 +0200

    fet: add verify modulith and write documentation
    Adds verify modulith architecture test and write documentation snippets

--- src/test/java/com/github/gaskapiotr/eventhub/EventHubApplicationTests.java
+++ src/test/java/com/github/gaskapiotr/eventhub/EventHubApplicationTests.java
@@ -2,12 +2,27 @@ package com.github.gaskapiotr.eventhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class EventHubApplicationTests {
+	ApplicationModules applicationModules = ApplicationModules.of(EventHubApplication.class);
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void verifyModulithArchitecture() {
+		applicationModules.verify();
+	}
+
+	@Test
+	void writeDocumentationSnippets() {
+		new Documenter(applicationModules)
+				.writeModulesAsPlantUml()
+				.writeIndividualModulesAsPlantUml();
 	}
 
 }
