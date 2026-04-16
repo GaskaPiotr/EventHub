189379f8259fb985173b7d3dac1c2179602f5801
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 15:00:34 +0200

    fet: add buy ticket logic and helper methods
    Adds buy ticket body and methods throw exception if unsuccessful, create
    and save ticket, create ticket and publish ticket purchased event

--- src/main/java/com/github/gaskapiotr/eventhub/ticketing/management/TicketingManagement.java
+++ src/main/java/com/github/gaskapiotr/eventhub/ticketing/management/TicketingManagement.java
@@ -1,14 +1,21 @@
 package com.github.gaskapiotr.eventhub.ticketing.management;
 
 import com.github.gaskapiotr.eventhub.catalog.CatalogInternalAPI;
+import com.github.gaskapiotr.eventhub.catalog.ReservationResponse;
+import com.github.gaskapiotr.eventhub.catalog.ReservationResult;
 import com.github.gaskapiotr.eventhub.ticketing.TicketDTO;
+import com.github.gaskapiotr.eventhub.ticketing.TicketPurchasedEvent;
 import com.github.gaskapiotr.eventhub.ticketing.TicketingExternalAPI;
 import com.github.gaskapiotr.eventhub.ticketing.mapper.TicketMapper;
+import com.github.gaskapiotr.eventhub.ticketing.model.Ticket;
 import com.github.gaskapiotr.eventhub.ticketing.repository.TicketRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -21,6 +28,41 @@ public class TicketingManagement implements TicketingExternalAPI {
     @Override
     @Transactional
     public TicketDTO buyTicket(Long workshopId, String attendeeEmail) {
-        // TODO
+        ReservationResponse response = catalogInternalAPI.reserveSeat(workshopId);
+        throwExceptionIfUnsuccessful(response, workshopId);
+        Ticket savedTicket = createAndSaveTicket(workshopId, attendeeEmail, response.price());
+        publishTicketPurchasedEvent(savedTicket.getId(), attendeeEmail, response);
+        return mapper.toDto(savedTicket);
+    }
+    
+    private void throwExceptionIfUnsuccessful(ReservationResponse response, Long workshopId) {
+        if (response.result() == ReservationResult.NOT_FOUND) {
+            throw new IllegalArgumentException("Workshop with ID: " + workshopId + " does not exists.");
+        } else if (response.result() == ReservationResult.SOLD_OUT) {
+            throw new IllegalStateException("Workshop " + response.workshopTitle() + " is completely sold out.");
+        }
+    }
+
+    private Ticket createAndSaveTicket(Long workshopId, String attendeeEmail, BigDecimal price) {
+        Ticket ticket = createTicket(workshopId, attendeeEmail, price);
+        return repository.save(ticket);
+    }
+
+    private Ticket createTicket(Long workshopId, String attendeeEmail, BigDecimal price) {
+        return Ticket.builder()
+                .workshopId(workshopId)
+                .attendeeEmail(attendeeEmail)
+                .purchaseDate(LocalDateTime.now())
+                .price(price)
+                .build();
+    }
+
+    public void publishTicketPurchasedEvent(Long ticketId, String attendeeEmail, ReservationResponse response) {
+        eventPublisher.publishEvent(new TicketPurchasedEvent(
+                ticketId,
+                attendeeEmail,
+                response.workshopTitle(),
+                response.price()
+        ));
     }
 }
