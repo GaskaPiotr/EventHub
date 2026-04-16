cfb07486e451b51a1d46e0b7922b897967784fae
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 14:26:33 +0200

    fet: add ticket repo extends jpa repo

--- src/main/java/com/github/gaskapiotr/eventhub/ticketing/repository/TicketRepository.java
+++ src/main/java/com/github/gaskapiotr/eventhub/ticketing/repository/TicketRepository.java
@@ -1,7 +1,9 @@
 package com.github.gaskapiotr.eventhub.ticketing.repository;
 
+import com.github.gaskapiotr.eventhub.ticketing.model.Ticket;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TicketRepository {
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
 }
