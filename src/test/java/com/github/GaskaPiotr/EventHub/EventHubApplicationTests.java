9bc4ec7a0002cb778cb929ad7bb6a65ad5a7b631
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 08:10:30 +0200

    Initial commit

---
+++ src/test/java/com/github/GaskaPiotr/EventHub/EventHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.GaskaPiotr.EventHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
