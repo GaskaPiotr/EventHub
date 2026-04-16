ff080a55a0fb79d1186acc429b7d1b5ed22c53cf
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 11:24:19 +0200

    fet: add ticket entity properties and annotations
    Adds id, workshop id, attendee email and purchase date to ticket
    Also adds getter, setter, no args constructor, all args constructor,
    builder to ticket and id, generated value to id

--- src/main/java/com/github/gaskapiotr/eventhub/ticketing/model/Ticket.java
+++ src/main/java/com/github/gaskapiotr/eventhub/ticketing/model/Ticket.java
@@ -1,7 +1,27 @@
 package com.github.gaskapiotr.eventhub.ticketing.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long workshopId;
+
+    private String attendeeEmail;
+    private LocalDateTime purchaseDate;
 }
