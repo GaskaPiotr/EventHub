f7556bf4d7290f28d24ffa9754a58ee10092e554
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 09:54:34 +0200

    fet: add workshop repo implements jpa repo

--- src/main/java/com/github/gaskapiotr/eventhub/catalog/repository/WorkshopRepository.java
+++ src/main/java/com/github/gaskapiotr/eventhub/catalog/repository/WorkshopRepository.java
@@ -1,7 +1,9 @@
 package com.github.gaskapiotr.eventhub.catalog.repository;
 
+import com.github.gaskapiotr.eventhub.catalog.model.Workshop;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WorkshopRepository {
+public interface WorkshopRepository extends JpaRepository<Workshop, Long> {
 }
