afadeace07f4500b26393a3ef853f00cbe937b57
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 10:09:14 +0200

    fet: add implementation and methods to override
    Adds repository and workshop fields
    Also adds methods to override and implements external and internal api

--- src/main/java/com/github/gaskapiotr/eventhub/catalog/management/CatalogManagement.java
+++ src/main/java/com/github/gaskapiotr/eventhub/catalog/management/CatalogManagement.java
@@ -1,7 +1,39 @@
 package com.github.gaskapiotr.eventhub.catalog.management;
 
+import com.github.gaskapiotr.eventhub.catalog.CatalogExternalAPI;
+import com.github.gaskapiotr.eventhub.catalog.CatalogInternalAPI;
+import com.github.gaskapiotr.eventhub.catalog.ReservationResult;
+import com.github.gaskapiotr.eventhub.catalog.WorkshopDTO;
+import com.github.gaskapiotr.eventhub.catalog.mapper.WorkshopMapper;
+import com.github.gaskapiotr.eventhub.catalog.repository.WorkshopRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
-public class CatalogManagement {
+@RequiredArgsConstructor
+public class CatalogManagement implements CatalogExternalAPI, CatalogInternalAPI {
+
+    private final WorkshopRepository repository;
+    private final WorkshopMapper mapper;
+
+    @Override
+    public List<WorkshopDTO> getAllWorkshops() {
+        // TODO
+    }
+
+    @Override
+    @Transactional
+    public WorkshopDTO createWorkshop(WorkshopDTO dto) {
+        // TODO
+    }
+
+    @Override
+    @Transactional
+    public ReservationResult reserveSeat(Long workshopId) {
+        // TODO
+    }
+
 }
