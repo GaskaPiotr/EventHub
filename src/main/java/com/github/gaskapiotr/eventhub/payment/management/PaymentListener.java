df01dd9820f94f4ede09f4147dd7486651549b40
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 09:03:15 +0200

    chore: add payment boilerplate
    Adds package private payment listener bean and service bean

---
+++ src/main/java/com/github/gaskapiotr/eventhub/payment/management/PaymentListener.java
@@ -0,0 +1,7 @@
+package com.github.gaskapiotr.eventhub.payment.management;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class PaymentListener {
+}
---
+++ src/main/java/com/github/gaskapiotr/eventhub/payment/management/PaymentManagement.java
@@ -0,0 +1,7 @@
+package com.github.gaskapiotr.eventhub.payment.management;
+
+import org.springframework.stereotype.Service;
+
+@Service
+class PaymentManagement {
+}
