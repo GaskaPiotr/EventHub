09b98fb17ad90d9e3bee6d3a1de66a7025a42a2d
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 09:24:16 +0200

    fet: add workshop properties and annotations
    Adds to workshop id, title, description, date, max capacity and current
    attendees
    Also adds lombok annotations getter, setter, no args constructor and all
    args constructor

--- src/main/java/com/github/gaskapiotr/eventhub/catalog/model/Workshop.java
+++ src/main/java/com/github/gaskapiotr/eventhub/catalog/model/Workshop.java
@@ -1,7 +1,24 @@
 package com.github.gaskapiotr.eventhub.catalog.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Workshop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String description;
+    private LocalDateTime date;
+    private int maxCapacity;
+    private int currentAttendees;
 }
