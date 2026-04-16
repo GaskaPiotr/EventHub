41d985607bc1522230cef9462affb50b24111d55
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 11:56:56 +0200

    fet: add table name to entities

--- src/main/java/com/github/gaskapiotr/eventhub/ticketing/model/Ticket.java
+++ src/main/java/com/github/gaskapiotr/eventhub/ticketing/model/Ticket.java
@@ -1,15 +1,13 @@
 package com.github.gaskapiotr.eventhub.ticketing.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "ticket")
 @Getter
 @Setter
 @NoArgsConstructor
