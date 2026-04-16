2003df75c2c2e35617b750890009542ede8d985e
GaskaPiotr <04piotr04@wp.pl>
2026-04-16 15:37:52 +0200

    fet: add boilerplate for ticket listener

--- src/main/java/com/github/gaskapiotr/eventhub/notification/management/NotificationListener.java
+++ src/main/java/com/github/gaskapiotr/eventhub/notification/management/NotificationListener.java
@@ -1,7 +1,17 @@
 package com.github.gaskapiotr.eventhub.notification.management;
 
+import com.github.gaskapiotr.eventhub.ticketing.TicketPurchasedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.modulith.events.ApplicationModuleListener;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 class NotificationListener {
+    private final NotificationListener notificationListener;
+
+    @ApplicationModuleListener
+    void on(TicketPurchasedEvent event) {
+        // TODO
+    }
 }
